8513fc3d84ac454b37a7e1c6aaf25566024e7845
aminulrony1999 <aminulrony1999@gmail.com>
2024-01-29 11:49:06 +0530

    delete operation added

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -3,10 +3,19 @@ import { useLoaderData } from 'react-router-dom';
 
 const Users = () => {
     const users = useLoaderData();
+     const handleDeleteUser = _id =>{
+        fetch(`http://localhost:5000/users/${_id}`,{
+            method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(data =>{
+            console.log(data);
+        })
+     }
     return (
         <div>
             {
-                users.map(user => <p key = {user._id}>Name : {user.name} <br /> Email : {user.email}</p>)
+                users.map(user => <p key = {user._id}>Name : {user.name} <br /> Email : {user.email} <button onClick={()=>{handleDeleteUser(user._id)}}>X</button></p>)
             }
         </div>
     );
