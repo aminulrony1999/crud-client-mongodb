9304342d67262ce388e7b09817d2c820ae9a4ba0
aminulrony1999 <aminulrony1999@gmail.com>
2024-01-30 02:20:18 +0530

    code updated

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import React from "react";
+import { useLoaderData } from "react-router-dom";
 
 const Users = () => {
-    const users = useLoaderData();
-     const handleDeleteUser = _id =>{
-        fetch(`http://localhost:5000/users/${_id}`,{
-            method: 'DELETE'
-        })
-        .then(res => res.json())
-        .then(data =>{
-            console.log(data);
-        })
-     }
-    return (
-        <div>
-            {
-                users.map(user => <p key = {user._id}>Name : {user.name} <br /> Email : {user.email} <button onClick={()=>{handleDeleteUser(user._id)}}>X</button></p>)
-            }
-        </div>
-    );
+  const users = useLoaderData();
+  const handleDeleteUser = (_id) => {
+    fetch(`http://localhost:5000/users/${_id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+  return (
+    <div>
+      {users.map((user) => (
+        <p key={user._id}>
+          Name : {user.name} <br /> Email : {user.email}{" "}
+          <button
+            onClick={() => {
+              handleDeleteUser(user._id);
+            }}
+          >
+            X
+          </button>
+        </p>
+      ))}
+    </div>
+  );
 };
 
 export default Users;
