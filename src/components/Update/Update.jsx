8d1e69e2d1210d5b6bcb0aeb598ff79ef59957d5
aminulrony1999 <aminulrony1999@gmail.com>
2024-01-30 20:49:28 +0530

    update form prepared

--- src/components/Update/Update.jsx
+++ src/components/Update/Update.jsx
@@ -2,13 +2,18 @@ import { useLoaderData } from 'react-router-dom';
 
 const Update = () => {
     const loadedUser = useLoaderData();
-    console.log(loadedUser);
+    const handleUpdate = event =>{
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+    }
     return (
         <div>
             <h3>Update information of {loadedUser.name}</h3>
-            <form>
+            <form onSubmit={handleUpdate}>
                 <input type="text" name="name" id="" defaultValue={loadedUser?.name} /> <br />
-                <input type="email" name="name" id="" defaultValue={loadedUser?.email} /> <br />
+                <input type="email" name="email" id="" defaultValue={loadedUser?.email} /> <br />
                 <input type="submit" value="Update" />
             </form>
         </div>
